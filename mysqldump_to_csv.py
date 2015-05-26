f10940522a16403befd973956eee46af34fa26a4
Vasia Korobkin <vasyakorobkin@mail.ru>
2015-05-26 14:13:39 +0300

    Bug-fix: processing empty strings in values

--- mysqldump_to_csv.py
+++ mysqldump_to_csv.py
@@ -51,6 +51,7 @@ def parse_values(values, outfile):
         for column in reader_row:
             # If our current string is empty...
             if len(column) == 0:
+                latest_row.append('""')
                 continue
             # If our string starts with an open paren
             if column[0] == "(":
