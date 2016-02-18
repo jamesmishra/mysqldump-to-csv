e05cc25513d242d873f3f8b9b015ca94920fedf3
Fil <fil@rezo.net>
2016-02-18 11:36:58 +0100

    allow large content
    allow large content in the dump by adjusting field_size_limit to the highest possible

--- mysqldump_to_csv.py
+++ mysqldump_to_csv.py
@@ -8,6 +8,9 @@ import sys
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE, SIG_DFL)
 
+# allow large content in the dump
+csv.field_size_limit(sys.maxsize)
+
 def is_insert(line):
     """
     Returns true if the line begins a SQL insert statement.
